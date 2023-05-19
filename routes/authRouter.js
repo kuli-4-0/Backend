53c04196f2e1abc42c69f8a1bfb2d04a3086262e
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2023-05-19 09:23:19 +0700

    menambahkan check user, dan resend verify

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, verify } = require('../controllers/authController');
+const {
+  register,
+  login,
+  verify,
+  resendVerification,
+} = require('../controllers/authController');
 
 router.post('/login', login);
 router.post('/register', register);
 router.get('/verify/:id', verify);
+router.post('/resendverify', resendVerification);
 
 module.exports = router;
