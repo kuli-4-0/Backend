d073a9e377bfb88a4e93733f1c4b69cc8b0febdf
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2023-05-20 20:49:05 +0700

    enable public folder

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const allRouter = require('./routes');
+const path = require('path');
 
 require('dotenv').config();
 const app = express();
+
+// Mengatur direktori public
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(allRouter);
 
