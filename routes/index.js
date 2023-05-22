16d739275683753dbfc311bf3d526c6228d274c3
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2023-05-22 15:25:27 +0700

    tambah welcome

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ const authRoute = require('./authRouter');
 const userRoute = require('./userRouter');
 const eventRoute = require('./eventRouter');
 
+router.get('/', (req, res) => {
+  res.send('<h1></h1>Welcome To Elise API</h1>');
+});
+
 router.use(authRoute);
 router.use('/users', userRoute);
 router.use('/events', eventRoute);
