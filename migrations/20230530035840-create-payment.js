c4837a180061a18b9f5cd0b8ae2c33bd3248ad17
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2023-06-05 11:10:16 +0700

    menambahkan tabel payment

---
+++ migrations/20230530035840-create-payment.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      liveRegistrationId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'LiveRegistrations',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      amount: {
+        type: Sequelize.FLOAT,
+      },
+      paymentMethod: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Payments');
+  },
+};
