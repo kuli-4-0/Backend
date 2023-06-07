a47d611fe6e92abd8d390a49d0dcbc9eb870c93f
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2023-06-08 02:12:12 +0700

    updet

--- config/index.js
+++ config/index.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 5000;
+const url = process.env.URL;
 
 module.exports = {
   host: process.env.HOST || host,
   port: process.env.PORT || port,
-  url: `http://${host}:${port}`,
+  url: url || `http://${host}:${port}`,
 };
